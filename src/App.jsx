823e20acd378f4d304eb3de261353057ac78e7e9
praveen-1101 <131389787+praveen-1101@users.noreply.github.com>
2023-04-21 20:45:31 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Notes from "./Components/Notes";
+const react = require("react");
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+export default App;
